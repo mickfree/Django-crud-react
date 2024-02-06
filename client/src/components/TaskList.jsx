7125d54b6937c5438b26cfdfe94892d8b405798b
mickfree <71110727@certus.edu.pe>
2024-02-05 22:03:24 -0500

    django-crud-react-vite-twdcss

---
+++ client/src/components/TaskList.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { getAllTask } from "../api/task.api";
+import {TaskCard} from './TaskCard'
+export function TaskList() {
+
+  const [task,setTask] = useState([]);
+    
+  useEffect(() => {
+    async function loadTask() {
+        const res = await getAllTask();
+        setTask(res.data)
+    }
+    loadTask();
+  }, []);
+
+  return (<div className="grid grid-cols-3 gap-3">
+    {task.map(task =>(
+      <TaskCard key={task.id} task={task}/>
+    ))}
+  </div>);
+}
---
+++ client/src/pages/TaskFormPage.jsx
@@ -0,0 +1,93 @@
+import { useForm } from "react-hook-form";
+import { createTask, deleteTask, updateTask, getTask } from "../api/task.api";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { toast } from "react-hot-toast";
+
+export function TaskFormPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm();
+  //Const
+  const navigate = useNavigate();
+  const params = useParams();
+
+  const onSubmit = handleSubmit(async (data) => {
+    if (params.id) {
+      const res = await updateTask(params.id, data);
+      console.log(res);
+    } else {
+      await createTask(data);
+      toast.success("Tarea Creada!", {
+        position: "top-center",
+        style: {
+          background: "#050504",
+          color: "#fff",
+        },
+      });
+    }
+    navigate("/tasks");
+  });
+
+  useEffect(() => {
+    async function loadTask() {
+      if (params.id) {
+        const res = await getTask(params.id);
+        console.log(res);
+        setValue("title", res.data.title);
+        setValue("description", res.data.description);
+      }
+    }
+    loadTask();
+  }, []);
+  return (
+    <div className="max-w-xl mx-auto">
+      <form onSubmit={onSubmit} className="bg-zinc-800 p-10 rounded-lg mt-2">
+        <input
+          type="text"
+          placeholder="Title"
+          {...register("title", { required: true })}
+          className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
+          autoFocus
+        />
+        {errors.title && <span>Title Required</span>}
+        <textarea
+          rows="3"
+          placeholder="Description"
+          {...register("description", { required: true })}
+          className="bg-zinc-700 p-3 rounded-lg block w-full"
+        ></textarea>
+        {errors.description && <span>Description Required</span>}
+        <button className="bg-indigo-500 p-3 rounded-lg block w-full mt-3">
+          Save
+        </button>
+      </form>
+      {params.id && (
+        <div className="flex justify-end">
+          <button
+            className="bg-red-500 p-3 rounded-lg w-48 mt-3"
+            onClick={async () => {
+              const accepted = window.confirm("Are you sure?");
+              if (accepted) {
+                await deleteTask(params.id);
+                toast.success("Task Removed", {
+                  position: "bottom-right",
+                  style: {
+                    background: "#101010",
+                    color: "#fff",
+                  },
+                });
+                navigate("/tasks");
+              }
+            }}
+          >
+            delete
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
